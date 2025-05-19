15e4a5127f11c903e1cefdb0980e5edf33368a95
01川川川 <3149696140@qq.com>
2025-05-19 15:38:05 +0800

    feat: init2

--- xiaolinshu-auth/src/main/java/com/puxinxiaolin/xiaolinshu/controller/TestController.java
+++ xiaolinshu-auth/src/main/java/com/puxinxiaolin/xiaolinshu/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.puxinxiaolin.xiaolinshu.controller;
 
+import com.puxinxiaolin.framework.biz.operationlog.aspect.ApiOperationLog;
+import com.puxinxiaolin.framework.common.response.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
