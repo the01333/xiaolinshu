d52363fc2f48c1c7f4c253cd98cf16664f2a5ca3
01川川川 <3149696140@qq.com>
2025-06-05 19:36:32 +0800

    feat: 关注接口开发

--- xiaolinshu-user-relation/xiaolinshu-user-relation-biz/src/main/java/com/puxinxiaolin/xiaolinshu/user/relation/biz/constant/RedisKeyConstants.java
+++ xiaolinshu-user-relation/xiaolinshu-user-relation-biz/src/main/java/com/puxinxiaolin/xiaolinshu/user/relation/biz/constant/RedisKeyConstants.java
@@ -3,9 +3,14 @@ package com.puxinxiaolin.xiaolinshu.user.relation.biz.constant;
 public class RedisKeyConstants {
     
     private static final String USER_FOLLOWING_KEY_PREFIX = "following:";
+    private static final String USER_FANS_KEY_PREFIX = "fans:";
     
     public static String buildUserFollowingKey(Long userId) {
         return USER_FOLLOWING_KEY_PREFIX + userId;
+    }
+
+    public static String buildUserFansKey(Long userId) {
+        return USER_FANS_KEY_PREFIX + userId;
     }
 
 }
--- xiaolinshu-user-relation/xiaolinshu-user-relation-biz/src/main/java/com/puxinxiaolin/xiaolinshu/user/relation/biz/consumer/FollowUnfollowConsumer.java
+++ xiaolinshu-user-relation/xiaolinshu-user-relation-biz/src/main/java/com/puxinxiaolin/xiaolinshu/user/relation/biz/consumer/FollowUnfollowConsumer.java
@@ -1,8 +1,10 @@
 package com.puxinxiaolin.xiaolinshu.user.relation.biz.consumer;
 
 import com.google.common.util.concurrent.RateLimiter;
+import com.puxinxiaolin.framework.common.util.DateUtils;
 import com.puxinxiaolin.framework.common.util.JsonUtils;
 import com.puxinxiaolin.xiaolinshu.user.relation.biz.constant.MqConstants;
+import com.puxinxiaolin.xiaolinshu.user.relation.biz.constant.RedisKeyConstants;
 import com.puxinxiaolin.xiaolinshu.user.relation.biz.domain.dataobject.FansDO;
 import com.puxinxiaolin.xiaolinshu.user.relation.biz.domain.dataobject.FollowingDO;
 import com.puxinxiaolin.xiaolinshu.user.relation.biz.domain.mapper.FansDOMapper;
@@ -13,10 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Objects;
 
 @Slf4j
@@ -32,6 +39,8 @@ public class FollowUnfollowConsumer implements RocketMQListener<Message> {
     private FansDOMapper fansDOMapper;
     @Resource
     private TransactionTemplate transactionTemplate;
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
     // 用令牌桶实现 MQ 流量削峰
     @Resource
     private RateLimiter rateLimiter;
@@ -93,8 +102,18 @@ public class FollowUnfollowConsumer implements RocketMQListener<Message> {
             return false;
         });
 
-        log.info("## 数据库添加记录结果: {}", isSuccess);
-        // TODO [YCcLin 2025/6/5]: 更新 Redis 中被关注用户的 ZSet 粉丝列表 
+        log.info("## 数据库添加记录结果: {}", isSuccess);    
+        // TODO [YCcLin 2025/6/5]: 更新 Redis 中被关注用户的 ZSet 粉丝列表
+        if (Boolean.TRUE.equals(isSuccess)) {
+            DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+            script.setScriptSource(new ResourceScriptSource(new ClassPathResource("/lua/follow_check_and_update_fans_zset.lua")));
+            script.setResultType(Long.class);
+
+            long timestamp = DateUtils.localDateTime2Timestamp(createTime);
+
+            String key = RedisKeyConstants.buildUserFansKey(followUserId);
+            redisTemplate.execute(script, Collections.singletonList(key), userId, timestamp);
+        }
     }
 
 }
